f036ca5c19efa9f4c381fd9e0cdfb93c98a8177d
kburnssr <50921973+kburnssr@users.noreply.github.com>
2019-12-03 15:03:48 -0800

    added hover over student pictures and a student.js page

---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+//making static assets
+app.use(express.static("public"));
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'students_db'
+});
+    
+connection.connect();
+
+app.get('/', function(req, res){
+ 
+  res.render("pages/home");
+ 
+});
+ 
+app.get('/students', function(req, res){
+	var q = `SELECT
+	c.id,
+	s.first_name,
+	s.last_name,
+	sp.image,
+	sp.country,
+	sp.bio,
+	c.tuition_fees AS amount_needed,
+	SUM(d.amount) AS raised,
+	c.semester,
+	c.year
+FROM campaign c
+INNER JOIN student s ON s.id = c.student_id
+LEFT JOIN student_profile sp ON sp.student_id = s.id
+LEFT JOIN donations d ON d.campaign_id = c.id
+WHERE c.end_date > NOW() AND s.deleted = 0
+GROUP BY c.id`
+	connection.query(q, function (error, results, fields) {
+		console.log(results);
+	  if (error) res.send(error)
+	  res.render("pages/students",{ data: results});
+	});
+});
+
+app.get('/about', function(req, res){
+	res.render("pages/about");
+});
+
+app.get('/students/:id', function(req, res){
+	var q = `SELECT
+		c.id,
+		s.first_name,
+		s.last_name,
+		sp.image,
+		sp.country,
+		sp.bio,
+		c.tuition_fees AS amount_needed,
+		SUM(d.amount) AS raised,
+		c.semester,
+		c.year
+	FROM campaign c
+	INNER JOIN student s ON s.id = c.student_id
+	LEFT JOIN student_profile sp ON sp.student_id = s.id
+	LEFT JOIN donations d ON d.campaign_id = c.id
+	WHERE c.end_date > NOW() 
+	AND s.deleted = 0
+	AND c.id = ?
+	GROUP BY c.id`
+	connection.query(q, [req.params.id], function (error, results, fields) {
+		console.log(results);
+	  if (error) res.send(error)
+	  res.render("pages/student",{ data: results[0]});
+	});
+});
+
+// by default the forms use req.query so let's not fight it
+//localhost:3000/insert?student_name=EuniceNjati
+app.get('/insert', function(req, res){
+	// res.json(req.query);
+
+	if (req.query.pres_name.length > 1){
+		connection.query('INSERT INTO student (student_name) VALUES (?)', [req.query.student_name], function (error, results, fields) {
+		  if (error) res.send(error)
+		  else res.redirect('/');
+		});
+	}else{
+		res.send('invalid name')
+	}
+});
+
+app.listen(5000, function(){
+	console.log('listening on 5000');
+});
+
+
+
+
+
+
+
+
+
